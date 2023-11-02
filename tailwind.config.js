2936aa0118e1eb395130278ee41fe8a027353c30
Fagbelu stephen <fagbelustephen@gmail.com>
2023-11-02 17:16:08 +0100

    update to tailwind.config and index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        util: {
+          "0%": { transform: "translateX(0)" },
+          "30%": { transform: "translateX(10rem)" },
+          "50%": { transform: "translateY(6rem) translateX(10rem)" },
+          "80%": { transform: "translateX(0) translateY(6rem)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        util: "util 14s ease-in infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
 
